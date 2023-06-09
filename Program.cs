1804d0e280d381cc643e4dadd132a52dc1d10ec5
Denis <denis-boz@hotmail.com>
2023-06-09 23:01:22 +0300

    Решение задачи 25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.Write("Введите число A: ");
+int numbA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numbB = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < numbB; i++)
+{
+    result *= numbA;
+}
+Console.WriteLine($"Число {numbA} в {numbB} степени равно {result}");
