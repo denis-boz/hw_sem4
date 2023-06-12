7c4e6e3a7cdb1f289e2de0f014c1ffa6bc390c14
Denis <denis-boz@hotmail.com>
2023-06-12 14:27:25 +0300

    Решение задачи 27

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
 2, 4 -> 16
 */
 
+/*
 Console.Write("Введите число A: ");
 int numbA = Convert.ToInt32(Console.ReadLine());
 
@@ -18,3 +19,22 @@ for (int i = 0; i < numbB; i++)
     result *= numbA;
 }
 Console.WriteLine($"Число {numbA} в {numbB} степени равно {result}");
+*/
+
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+
+Console.Write("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (numb > 0)
+{
+    result += numb % 10;
+    numb = numb / 10;
+}
+Console.WriteLine($"Сумма всех цифр равна {result} ");
